65b520ec6ae0a55e578931d46ec29e6663e9f9d6
Dominic Dawes <dominic.d.dawes@gmail.com>
2024-11-02 20:57:39 -0400

    add retry logic

--- tasks/celery_app.py
+++ tasks/celery_app.py
@@ -40,6 +40,7 @@ celery_app.conf.update(
     result_serializer='json',
     timezone='UTC',
     enable_utc=True,
+    broker_connection_retry_on_startup=True,  # Added setting
 )
 
 # Import tasks to register them with Celery (THIS WORKS!!!)
